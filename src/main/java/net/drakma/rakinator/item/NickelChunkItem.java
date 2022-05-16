500007b5d6876e9d4697353d594724559fa0f795
Drakma <caseyathompson@hotmail.com>
2022-05-16 15:52:43 -0500

    ingots added

---
+++ src/main/java/net/drakma/rakinator/item/NickelChunkItem.java
@@ -0,0 +1,25 @@
+
+package net.drakma.rakinator.item;
+
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import net.drakma.rakinator.init.RakinatorModTabs;
+
+public class NickelChunkItem extends Item {
+	public NickelChunkItem() {
+		super(new Item.Properties().tab(RakinatorModTabs.TAB_RAKINATOR_CREATIVE_TAB).stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.EAT;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 0;
+	}
+}
